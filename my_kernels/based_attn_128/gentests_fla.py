d810ca58605f07b6308c45d748e805364c7c94c9
alxndrTL <alextorresleguet@icloud.com>
2024-12-11 19:04:38 +0000

    further tests

---
+++ my_kernels/based_attn_128/gentests_fla.py
@@ -0,0 +1,66 @@
+import torch
+from tqdm import trange
+import numpy as np
+import sys
+from einops import rearrange
+import math
+
+# only generate a single batch/head of data, which makes file loading much faster.
+# it does mean we'll have to check batch/head behavior separately later, but that should be much easier to debug.
+B = 1
+H = 1
+N = 1024
+D = 32
+DV = 32
+
+TESTNAME = sys.argv[1]
+
+if TESTNAME in ['ones_all', 'ones_t0', 'ones_t1', 'ones_t0t1', 'ones_t2']:
+    q = (torch.ones((B, H, N, D), dtype=torch.bfloat16, device='cuda')/D).to(torch.float32)
+    k = (torch.ones((B, H, N, D), dtype=torch.bfloat16, device='cuda')/D).to(torch.float32)
+    v = (torch.ones((B, H, N, DV), dtype=torch.bfloat16, device='cuda')/DV).to(torch.float32)
+elif TESTNAME in ['randn_all', 'randn_t0', 'randn_t1', 'randn_t0t1', 'randn_t2']:
+    torch.random.manual_seed(42)
+    q = (torch.randn((B, H, N, D), dtype=torch.bfloat16, device='cuda')/float(D)**.5).to(torch.float32)
+    k = (torch.randn((B, H, N, D), dtype=torch.bfloat16, device='cuda')/float(D)**.5).to(torch.float32)
+    v = (torch.randn((B, H, N, DV), dtype=torch.bfloat16, device='cuda')/DV).to(torch.float32)
+else:
+    print('Invalid test name')
+    sys.exit(0)
+
+def pytorch_test_fla(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    scale = None,
+):
+    if scale is None:
+        scale = q.shape[-1] ** -0.5
+    q = q * scale
+    attn = q @ k.transpose(-2, -1)
+    #attn = 1 + attn + 1/2 * (attn ** 2)
+    attn.masked_fill_(~torch.tril(torch.ones(
+        q.shape[-2], q.shape[-2], dtype=torch.bool, device=q.device)), 0)
+    o = attn @ v
+    return o
+
+o = pytorch_test_fla(q, k, v, scale=True)
+
+with open(f'{TESTNAME}_{B}x{H}x{N}x{D}x{DV}.txt', 'w') as f:
+    qf = q.to(torch.float32).flatten().cpu().numpy().tolist()
+    kf = k.to(torch.float32).flatten().cpu().numpy().tolist()
+    vf = v.to(torch.float32).flatten().cpu().numpy().tolist()
+    of = o.to(torch.float32).flatten().cpu().numpy().tolist()
+
+    for i in trange(B*H*N*D):
+        f.write(repr(qf[i]))
+        f.write(' ')
+    for i in trange(B*H*N*D):
+        f.write(repr(kf[i]))
+        f.write(' ')
+    for i in trange(B*H*N*DV):
+        f.write(repr(vf[i]))
+        f.write(' ')
+    for i in trange(B*H*N*DV):
+        f.write(repr(of[i]))
+        f.write(' ')
